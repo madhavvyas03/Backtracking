767f93b204ba18dc6a9ff03c70024d5183da664f
madhavvyas03 <85939142+madhavvyas03@users.noreply.github.com>
2022-01-17 13:29:15 +0530

    Update knight_tour_problem.c

--- knight_tour_problem.c
+++ knight_tour_problem.c
@@ -1,25 +1,13 @@
-/* The knight is placed on the first block of an empty
- * board and, moving according to the rules of chess, must
- * visit each square exactly once.
- */
-
- 
-// C program for Knight Tour problem 
 #include<stdio.h> 
 #define N 8 
   
 int solveKTUtil(int x, int y, int movei, int sol[N][N], 
                 int xMove[],  int yMove[]); 
-  
-/* A utility function to check if i,j are valid indexes 
-   for N*N chessboard */
 bool isSafe(int x, int y, int sol[N][N]) 
 { 
     return ( x >= 0 && x < N && y >= 0 && 
              y < N && sol[x][y] == -1); 
 } 
-  
-/* A utility function to print solution matrix sol[N][N] */
 void printSolution(int sol[N][N]) 
 { 
     for (int x = 0; x < N; x++) 
@@ -29,34 +17,15 @@ void printSolution(int sol[N][N])
         printf("\n"); 
     } 
 } 
-  
-/* This function solves the Knight Tour problem using 
-   Backtracking.  This function mainly uses solveKTUtil() 
-   to solve the problem. It returns false if no complete 
-   tour is possible, otherwise return true and prints the 
-   tour. 
-   Please note that there may be more than one solutions, 
-   this function prints one of the feasible solutions.  */
 bool solveKT() 
 { 
     int sol[N][N]; 
-  
-    /* Initialization of solution matrix */
     for (int x = 0; x < N; x++) 
         for (int y = 0; y < N; y++) 
             sol[x][y] = -1; 
-  
-    /* xMove[] and yMove[] define next move of Knight. 
-       xMove[] is for next value of x coordinate 
-       yMove[] is for next value of y coordinate */
     int xMove[8] = {  2, 1, -1, -2, -2, -1,  1,  2 }; 
     int yMove[8] = {  1, 2,  2,  1, -1, -2, -2, -1 }; 
-  
-    // Since the Knight is initially at the first block 
     sol[0][0]  = 0; 
-  
-    /* Start from 0,0 and explore all tours using 
-       solveKTUtil() */
     if (solveKTUtil(0, 0, 1, sol, xMove, yMove) == false) 
     { 
         printf("Solution does not exist"); 
@@ -68,16 +37,12 @@ bool solveKT()
     return true; 
 } 
   
-/* A recursive utility function to solve Knight Tour 
-   problem */
 int solveKTUtil(int x, int y, int movei, int sol[N][N], 
                 int xMove[N], int yMove[N]) 
 { 
    int k, next_x, next_y; 
    if (movei == N*N) 
-       return true; 
-  
-   /* Try all next moves from the current coordinate x, y */
+       return true;
    for (k = 0; k < 8; k++) 
    { 
        next_x = x + xMove[k]; 
